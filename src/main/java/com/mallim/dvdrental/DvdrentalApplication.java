8e4f0521ff7c630ffd14e8abe56090ab6ce58961
Ian LIM <mallim.ink@gmail.com>
2023-06-14 15:32:17 +0800

    Fixes #6 Added Problem: Spring Web MVC https://github.com/zalando/problem-spring-web/tree/main/problem-spring-web

--- src/main/java/com/mallim/dvdrental/DvdrentalApplication.java
+++ src/main/java/com/mallim/dvdrental/DvdrentalApplication.java
@@ -2,8 +2,9 @@ package com.mallim.dvdrental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class DvdrentalApplication {
     public static void main(String[] args) {
         SpringApplication.run(DvdrentalApplication.class, args);
